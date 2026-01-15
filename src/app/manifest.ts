01062120c3755dc429e3e0e8b37ef9e8d6593737
ZephyrGraphic <fenrisulfr696@gmail.com>
2026-01-15 08:01:47 +0700

    fix query string couple photo v2

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#ee2b5b',
     icons: [
       {
-        src: '/images/couple.jpg',
+        src: '/images/couple.jpg?v=2',
         sizes: '192x192',
         type: 'image/jpeg',
       },
       {
-        src: '/images/couple.jpg',
+        src: '/images/couple.jpg?v=2',
         sizes: '512x512',
         type: 'image/jpeg',
       },
